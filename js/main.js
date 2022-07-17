292da973f2447092173aeae7d5fdcdf76d036152
muriloapolo <massps2@gmail.com>
2022-07-17 19:18:56 -0300

    melhorias

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
-navToggle.addEventListener('click', function(){
+navToggle.addEventListener('click', function () {
     // console.log(links.classList)
     // console.log(links.classList.contains('random'))
     // console.log(links.classList.contains('links'))
@@ -13,7 +13,29 @@ navToggle.addEventListener('click', function(){
     links.classList.toggle('show-links');
 })
 
+//navBlog-principal
+//blog
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM completamente carregado e analisado");
-  });
+
+
+const btnSide = document.getElementById('sideHome')
+btnSide.addEventListener('click', exibeBarra)
+
+function exibeBarra() {
+
+    const barraLateral = document.querySelector('.sidebar');
+    const navBar = document.querySelector('.navBlog-principal');
+    const blog = document.querySelector('.blog');
+    if (!blog.classList.contains('origem')) {
+        barraLateral.classList.add("sidebarRespNone")
+        blog.classList.add('origem')
+        navBar.classList.add('origem')       
+    }
+   
+     else {
+      
+        blog.classList.remove('origem')
+        navBar.classList.remove('origem')
+        barraLateral.classList.remove('sidebarRespNone')
+    }
+}
