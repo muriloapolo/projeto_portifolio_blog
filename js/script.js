bafd90abf8b2a8abc68c1cbcc4b939353062f022
muriloapolo <massps2@gmail.com>
2022-07-23 22:03:54 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const btnResp = document.querySelector('.btnResponsive');
 btnResp.addEventListener('click', menuResponsivo);
 
 function menuResponsivo(){
-    // const barraNavegacao = document.querySelector('.navPrincipal');
     const listaMenu = document.querySelector('.navList') 
     listaMenu.classList.toggle('show-links')
    
